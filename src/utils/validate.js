c822d5565cb75265b8c61c33fe726056cb9b9fc5
luckyadam <weitaozsh@gmail.com>
2018-02-26 22:43:24 +0800

    feat: 用户信息修改增加提交时增加校验

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,7 @@
 export const phoneReg = /^1[3|4|5|8][0-9]\d{8}$/
 export const identifyIdReg = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/
 export const urlReg = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
+export const faxReg = /(^(\d{2,4}[-_－—]?)?\d{3,8}([-_－—]?\d{3,8})?([-_－—]?\d{1,7})?$)|(^0?1\d{10}$)/
 
 export function isEmptyStr (str) {
   return (!str || str.length === 0)
@@ -8,6 +9,10 @@ export function isEmptyStr (str) {
 
 export function isValidPhoneNumber (number) {
   return phoneReg.test(number)
+}
+
+export function isValidFax (fax) {
+  return faxReg.test(fax)
 }
 
 export function isValidIdentifyId (id) {
